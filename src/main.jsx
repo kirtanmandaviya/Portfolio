74a064c0c0e3cd1c5f2797bbe35ee00b1f5460e9
MRKP3110 <161864051+MRKP3110@users.noreply.github.com>
2025-12-27 20:43:46 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const basename =
+  import.meta.env.MODE === 'production' ? '/Portfolio' : '/'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
